e9485599539233c2c5c43c15b232e7e2a5a5875a
Nathan-Camredon <nathan.camredon@laplateforme.io>
2025-12-01 12:07:11 +0100

    Suppression de caca.py et update morpion

--- morpion_pg.py
+++ morpion_pg.py
@@ -1,14 +1,37 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))  #Résolution
+screen = pygame.display.set_mode((1280, 720))
 running = True
-
+#------------------------------------------------------------------------------------------------------------------------
+#                   VARIABLE
+#------------------------------------------------------------------------------------------------------------------------
+clock = pygame.time.Clock()     #Horloge pour limiteus ips (pas touche Nath)
+#--- couleur ---
+black = (0, 0, 0)
+white = (255, 255, 255)
+#--- Valeurs cases ----
+grille_data = [
+    [None, None, None],  # Ligne 0
+    [None, None, None],  # Ligne 1
+    [None, None, None]   # Ligne 2
+]
 #------------------------------------------------------------------------------------------------------------------------
 #                   FONCTION
 #------------------------------------------------------------------------------------------------------------------------
 
 def Grille_jeux():
-    pygame.draw.line(black)
+    # --- Lignes Verticales ---
+    pygame.draw.line(screen, black, (415, 135), (415, 585), 5)
+    pygame.draw.line(screen, black, (565, 135), (565, 585), 5)
+    pygame.draw.line(screen, black, (715, 135), (715, 585), 5)
+    pygame.draw.line(screen, black, (865, 135), (865, 585), 5)
+
+    # --- Lignes Horizontales ---
+    pygame.draw.line(screen, black, (415, 135), (865, 135), 5)
+    pygame.draw.line(screen, black, (415, 285), (865, 285), 5)
+    pygame.draw.line(screen, black, (415, 435), (865, 435), 5)
+    pygame.draw.line(screen, black, (415, 585), (865, 585), 5)
+    
 
 
 #------------------------------------------------------------------------------------------------------------------------
@@ -20,8 +43,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
+    screen.fill(white)
+    Grille_jeux()
 
 
 
