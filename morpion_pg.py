7d74e8b93fce12357597c15b9745eb428cc051b6
Nathan-Camredon <nathan.camredon@laplateforme.io>
2025-12-01 14:15:59 +0100

    main

--- morpion_pg.py
+++ morpion_pg.py
@@ -9,12 +9,16 @@ clock = pygame.time.Clock()     #Horloge pour limiteus ips (pas touche Nath)
 #--- couleur ---
 black = (0, 0, 0)
 white = (255, 255, 255)
+rouge = (255, 0, 0)
+bleu = (0, 0, 255)
 #--- Valeurs cases ----
 grille_data = [
-    [None, None, None],  # Ligne 0
-    [None, None, None],  # Ligne 1
-    [None, None, None]   # Ligne 2
+    [None, None, None],  
+    [None, None, None], 
+    [None, None, None]   
 ]
+#--- Tours du joueurs --- 
+Tour = "X"
 #------------------------------------------------------------------------------------------------------------------------
 #                   FONCTION
 #------------------------------------------------------------------------------------------------------------------------
@@ -43,12 +47,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                (x, y) = pygame.mouse.get_pos()
+                if x > 415 and x < 865 and y > 135 and y < 585:
+                    colonne = (x - 415) // 150
+                    ligne = (y - 135) // 150
+                    if grille_data[ligne][colonne] == None:
+                        grille_data[ligne][colonne] = Tour 
+                        if Tour == "X":
+                            Tour = "O"
+                        else:
+                            Tour = "X"
+                print(grille_data)
+
+
     screen.fill(white)
     Grille_jeux()
-
-
-
-
 
     pygame.display.flip()
 
