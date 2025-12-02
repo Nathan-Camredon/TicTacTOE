bfea53c1f42806289c51921dda907f94c04a0df0
Nathan-Camredon <nathan.camredon@laplateforme.io>
2025-12-02 08:44:57 +0100

    Correction des conflits et mise à jour

--- morpion_pg.py
+++ morpion_pg.py
@@ -39,15 +39,12 @@ def Grille_jeux():
 
 
 #------------------------------------------------------------------------------------------------------------------------
-#                   LANCEMENT
+#                   LANCEMENT.
 #------------------------------------------------------------------------------------------------------------------------
 
 
 while running:
-<<<<<<< HEAD
     screen.fill(white)
-=======
->>>>>>> 7d74e8b93fce12357597c15b9745eb428cc051b6
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -64,7 +61,6 @@ while running:
                         else:
                             Tour = "X"
                 print(grille_data)
-<<<<<<< HEAD
     for i in range(3):
         for j in range(3):
             if grille_data[i][j] == "X":
@@ -73,11 +69,6 @@ while running:
             elif grille_data[i][j] == "O":
                 pygame.draw.circle(screen, bleu, (490 + j * 150, 210 + i * 150), 60, 5)
 
-=======
-
-
-    screen.fill(white)
->>>>>>> 7d74e8b93fce12357597c15b9745eb428cc051b6
     Grille_jeux()
 
     pygame.display.flip()
